f3065b6e538f86302a52d98f5cbf213347166b14
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-12-24 15:06:08 -0300

    new groups

--- modules/access-review/main.tf
+++ modules/access-review/main.tf
@@ -1,16 +1,14 @@
 data "azuread_client_config" "current" {}
 
 locals {
-  owner1   = "Owner1"
-  member1  = "Member1"
   password = "cvds89avF!sd980vzx"
 }
 
+### Users ###
 resource "azuread_user" "owner1" {
   account_enabled     = true
-  user_principal_name = "${local.owner1}@${var.entraid_tenant_domain}"
-  display_name        = local.owner1
-  mail_nickname       = local.owner1
+  user_principal_name = "Owner1@${var.entraid_tenant_domain}"
+  display_name        = "Owner1"
   password            = local.password
 }
 
@@ -22,13 +20,31 @@ resource "azuread_group" "group1" {
 
 resource "azuread_user" "member1" {
   account_enabled     = true
-  user_principal_name = "${local.member1}@${var.entraid_tenant_domain}"
-  display_name        = local.member1
-  mail_nickname       = local.member1
+  user_principal_name = "Member1@${var.entraid_tenant_domain}"
+  display_name        = "Member1"
   password            = local.password
 }
 
 resource "azuread_group_member" "member1" {
   group_object_id  = azuread_group.group1.id
   member_object_id = azuread_user.member1.id
+}
+
+### Reviewers ###
+resource "azuread_group" "reviewers1" {
+  display_name     = "Reviewers1"
+  owners           = [azuread_user.owner1.object_id]
+  security_enabled = true
+}
+
+resource "azuread_user" "reviewer1" {
+  account_enabled     = true
+  user_principal_name = "Reviewer1@${var.entraid_tenant_domain}"
+  display_name        = "Reviewer1"
+  password            = local.password
+}
+
+resource "azuread_group_member" "reviewers1" {
+  group_object_id  = azuread_group.reviewers1.id
+  member_object_id = azuread_user.reviewer1.id
 }
